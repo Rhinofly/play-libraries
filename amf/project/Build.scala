3fb16882906828aeee8379971f6ff6b431168bfe
ewestra <ewestra@rhinofly.nl>
2012-04-26 16:12:34 +0200

    Fixed some settings

--- amf/project/Build.scala
+++ amf/project/Build.scala
@@ -12,7 +12,7 @@ object ApplicationBuild extends Build {
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
+      organization := "nl.rhinofly"    
     )
 
 }
