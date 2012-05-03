3b582da3bc50ae8d4dd11c0e5832747888e54cbc
ewestra <ewestra@rhinofly.nl>
2012-05-03 15:51:16 +0200

    Changed the playConfiguration method to throw an error if the configuration entry was not found

--- apis/utils/app/fly/play/utils/PlayUtils.scala
+++ apis/utils/app/fly/play/utils/PlayUtils.scala
@@ -5,11 +5,12 @@ import play.api.libs.ws.WS
 import play.api.libs.ws.Response
 import play.api.libs.concurrent.Promise
 import play.api.mvc.Call
+import play.api.PlayException
 
 object PlayUtils {
 
-  def playConfiguration(key: String)(implicit app: Application): Option[String] =
-    app.configuration.getString(key)
+  def playConfiguration(key: String)(implicit app: Application): String =
+    app.configuration.getString(key).getOrElse(throw PlayException("Configuration error", "Could not find " + key + " in settings"))
 
   def ws[T](url: String)(handler: Response => T): Promise[T] =
     WS.url(url).get().map(handler(_))
