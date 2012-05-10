8c54b5ac255ac2cb7db74408faec7a4123359ed8
ewestra <ewestra@rhinofly.nl>
2012-05-10 11:39:08 +0200

    Cleanup of projects and proper publish settings

--- amf/project/Build.scala
+++ amf/project/Build.scala
@@ -6,21 +6,14 @@ import scala.io.Source
 object ApplicationBuild extends Build {
 
   val appName = "amf"
-  val appVersion = "1.0"
+  val appVersion = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
     "com.exadel.flamingo.flex" % "amf-serializer" % "1.5.0")
 
-  lazy val loadedCredentials = {
-    val line = Source.fromFile("credentials").getLines.toSeq
-    Credentials(line(0), line(1), line(2), line(3))
-  }
-    
-  val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+  val main = PlayProject(appName, appVersion, appDependencies).settings(
     organization := "nl.rhinofly",
-
-    publishTo := Some(loadedCredentials.realm at loadedCredentials.host),
-
-    credentials += loadedCredentials)
-
+    publishTo := Some("Rhinofly Internal Repository" at "http://maven-repository.rhinofly.net:8081/artifactory/libs-snapshot-local"),
+    credentials += Credentials(Path.userHome / ".ivy2" / ".credentials")
+  )
 }
