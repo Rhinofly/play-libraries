aa6a6a3769298f62cdf73e943b7a18515a06587e
ewestra <ewestra@rhinofly.nl>
2012-06-01 14:36:23 +0200

    Changed version to release

--- apis/jira/project/Build.scala
+++ apis/jira/project/Build.scala
@@ -5,7 +5,7 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
     val appName         = "api-jira"
-    val appVersion      = "1.0-SNAPSHOT"
+    val appVersion      = "1.0"
 
     val appDependencies = Seq(
     "nl.rhinofly" %% "library-utils" % "1.0")
