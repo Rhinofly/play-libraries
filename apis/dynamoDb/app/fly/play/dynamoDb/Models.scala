20745ae5cacf7855bbb80932f1e5d6ec359b2013
ewestra <ewestra@rhinofly.nl>
2012-06-12 18:04:57 +0200

    Added CreateTableRequest

--- apis/dynamoDb/app/fly/play/dynamoDb/Models.scala
+++ apis/dynamoDb/app/fly/play/dynamoDb/Models.scala
@@ -6,36 +6,87 @@ import play.api.libs.json.Reads
 import play.api.libs.json.Writes
 import play.api.libs.json.JsObject
 import play.api.libs.json.JsString
+import play.api.libs.json.JsObject
 
 trait DefaultWrites {
-
-
   implicit object IntWrites extends Writes[Int] {
     def writes(o: Int) = JsString(o.toString)
   }
 }
 
 trait JsonUtils {
-	def key[T](name: String)(value: T)(implicit wrt: Writes[T]): (String, JsValue) = name -> toJson(value)
-			
-			def optional[T](name: String, option: Option[T])(implicit wrt: Writes[T]): List[(String, JsValue)] = option.map(key(name)).toList
+  def key[T](name: String)(value: T)(implicit wrt: Writes[T]): (String, JsValue) =
+    name -> toJson(value)
+
+  def optional[T](name: String, option: Option[T])(implicit wrt: Writes[T]): List[(String, JsValue)] =
+    option.map(key(name)).toList
 }
 
+/*
+ * SUPPORT CLASSES
+ */
+
+case class ProvisionedThroughPut(writeCapacityUnits: Int = 5, readCapacityUnits: Int = 10)
+
+object ProvisionedThroughPut extends ((Int, Int) => ProvisionedThroughPut) {
+  implicit object ProvisionedThroughPutWrites extends Writes[ProvisionedThroughPut] {
+    def writes(p: ProvisionedThroughPut): JsValue = JsObject(Seq(
+      "ReadCapacityUnits" -> toJson(p.readCapacityUnits),
+      "WriteCapacityUnits" -> toJson(p.writeCapacityUnits)))
+  }
+}
+
+sealed abstract class AttributeType(val value: String)
+
+case object S extends AttributeType("S")
+case object N extends AttributeType("N")
+
+case class Attribute(name: String, tpe: AttributeType) {
+  require(name.length > 0, "The given name should have length")
+  require(name.length < 256, "The given name has a length that exceeds 255")
+}
+
+object Attribute extends ((String, AttributeType) => Attribute) {
+  implicit object AttributeWrites extends Writes[Attribute] {
+    def writes(a: Attribute): JsValue = JsObject(Seq(
+      "AttributeName" -> toJson(a.name),
+      "AttributeType" -> toJson(a.tpe.value)))
+  }
+}
+
+case class KeySchema(hashKeyElement: Attribute, rangeKeyElement: Option[Attribute] = None)
+
+object KeySchema extends ((Attribute, Option[Attribute]) => KeySchema) {
+  implicit object KeySchemaWrites extends Writes[KeySchema] with JsonUtils {
+    def writes(k: KeySchema): JsValue = JsObject(List(
+      "HashKeyElement" -> toJson(k.hashKeyElement)) :::
+      optional("RangeKeyElement", k.rangeKeyElement))
+  }
+}
+
+/*
+ * EXCEPTIONS
+ */
+
 trait DynamoDbException {
-  def message:String
+  def message: String
 }
 
 object DynamoDbException {
   implicit object DynamoDbExceptionReads extends Reads[DynamoDbException] {
-    def reads(json:JsValue) = convert((json \ "__type").as[String], (json \ "Message").as[String])
-    
-    def convert(tpe:String, message:String):DynamoDbException = tpe match {
+    def reads(json: JsValue) = convert((json \ "__type").as[String], (json \ "Message").as[String])
+
+    def convert(tpe: String, message: String): DynamoDbException = tpe match {
       case "com.amazon.coral.service#SerializationException" => SerializationException(message)
     }
   }
 }
 
-case class SerializationException(message:String) extends DynamoDbException
+case class SerializationException(message: String) extends DynamoDbException
+
+/*
+ * LIST TABLES
+ */
 
 case class ListTablesRequest(limit: Option[Int] = None, exclusiveStartTableName: Option[String] = None)
 
@@ -54,5 +105,23 @@ object ListTablesResponse extends ((Seq[String], Option[String]) => ListTablesRe
     def reads(json: JsValue) = ListTablesResponse(
       (json \ "TableNames").as[Seq[String]],
       (json \ "LastEvaluatedTableName").asOpt[String])
+  }
+}
+
+/*
+ * CREATE TABLE
+ */
+case class CreateTableRequest(name: String, keySchema: KeySchema, provisionedThroughPut: ProvisionedThroughPut = ProvisionedThroughPut()) {
+  require(name.length > 2, "The given name must have a length of at least 3")
+  require(name.length < 256, "The given name has a length that exceeds 255")
+  require("""^[a-zA-Z0-9_\-\.]*$""".r.pattern.matcher(name).matches, "Allowed characters for table names are a-z, A-Z, 0-9, '_' (underscore), '-' (dash), and '.' (dot).")
+}
+
+object CreateTableRequest extends ((String, KeySchema, ProvisionedThroughPut) => CreateTableRequest) {
+  implicit object CreateTableRequestWrites extends Writes[CreateTableRequest] {
+    def writes(r: CreateTableRequest): JsValue = JsObject(Seq(
+      "TableName" -> toJson(r.name),
+      "KeySchema" -> toJson(r.keySchema),
+      "ProvisionedThroughput" -> toJson(r.provisionedThroughPut)))
   }
 }
--- apis/dynamoDb/test/fly/play/dynamoDb/DynamoDbSpec.scala
+++ apis/dynamoDb/test/fly/play/dynamoDb/DynamoDbSpec.scala
@@ -1,45 +1,80 @@
 package fly.play.dynamoDb
 
-import org.specs2.mutable.{Specification, Before}
+import org.specs2.mutable.{ Specification, Before }
 import play.api.test.FakeApplication
 import fly.play.sts.AwsSessionCredentials
 import fly.play.aws.auth.AwsCredentials
-import play.api.libs.json.Json.{toJson, parse, fromJson}
+import play.api.libs.json.Json.{ toJson, parse, fromJson }
 import play.api.libs.json.JsValue
 
 object DynamoDbSpec extends Specification with Before {
-	def f = FakeApplication(new java.io.File("./test/"))
+  def f = FakeApplication(new java.io.File("./test/"))
   def before = play.api.Play.start(f)
-  
-  
+
   "DynamoDb" in {
-	  "list tables" in {
-    "ListTableRequest should create correct json" in {
-	    toJson(ListTablesRequest()) must_== toJson(Map[String, JsValue]()) 
-	    toJson(ListTablesRequest(limit = Some(10))) must_== parse("""{ "Limit":10 }""")
-	    toJson(ListTablesRequest(exclusiveStartTableName = Some("Test"))) must_== parse("""{"ExclusiveStartTableName":"Test"}""") 
-	    toJson(ListTablesRequest(Some(10), Some("Test"))) must_== parse("""{"Limit":10, "ExclusiveStartTableName":"Test"}""") 
-	  }
-    
-    	"ListTableResponse should be created from json" in {
-    	  fromJson[ListTablesResponse](parse("""{"TableNames":["Table1","Table2","Table3"], "LastEvaluatedTableName":"Table3"}""")) must beLike {
-    	    case ListTablesResponse(Seq("Table1", "Table2", "Table3"), Some("Table3")) => ok
-    	  }
-    	  fromJson[ListTablesResponse](parse("""{"TableNames":["Table1","Table2","Table3"]}""")) must beLike {
-    	  	case ListTablesResponse(Seq("Table1", "Table2", "Table3"), None) => ok
-    	  }
-    	    
-    	}
-    	
-    	"make a successfull request" in {
-    	  DynamoDb.listTables(ListTablesRequest(Some(10))).value.get must beLike {
-    	    case Right(ListTablesResponse(Seq("Test"), None)) => ok
-    	  }
-    	  DynamoDb(ListTablesRequest(Some(10))).value.get must beLike {
-    	  case Right(ListTablesResponse(Seq("Test"), None)) => ok
-    	  }
-    	}
-	  }
-	  
-	}
+    "list tables" in {
+      "ListTableRequest should create correct json" in {
+        toJson(ListTablesRequest()) must_== toJson(Map[String, JsValue]())
+        toJson(ListTablesRequest(limit = Some(10))) must_== parse("""{ "Limit":10 }""")
+        toJson(ListTablesRequest(exclusiveStartTableName = Some("Test"))) must_== parse("""{"ExclusiveStartTableName":"Test"}""")
+        toJson(ListTablesRequest(Some(10), Some("Test"))) must_== parse("""{"Limit":10, "ExclusiveStartTableName":"Test"}""")
+      }
+
+      "ListTableResponse should be created from json" in {
+        fromJson[ListTablesResponse](parse("""{"TableNames":["Table1","Table2","Table3"], "LastEvaluatedTableName":"Table3"}""")) must beLike {
+          case ListTablesResponse(Seq("Table1", "Table2", "Table3"), Some("Table3")) => ok
+        }
+        fromJson[ListTablesResponse](parse("""{"TableNames":["Table1","Table2","Table3"]}""")) must beLike {
+          case ListTablesResponse(Seq("Table1", "Table2", "Table3"), None) => ok
+        }
+
+      }
+
+      "make a successfull request" in {
+        DynamoDb.listTables(ListTablesRequest(Some(10))).value.get must beLike {
+          case Right(ListTablesResponse(Seq("Test"), None)) => ok
+        }
+        DynamoDb(ListTablesRequest(Some(10))).value.get must beLike {
+          case Right(ListTablesResponse(Seq("Test"), None)) => ok
+        }
+      }
+    }
+
+    "create table" in {
+      "ProvisionedThroughput" in {
+        "should create correct json" in {
+          toJson(ProvisionedThroughPut()) must_== parse("""{"ReadCapacityUnits":10,"WriteCapacityUnits":5}""")
+          toJson(ProvisionedThroughPut(10, 5)) must_== parse("""{"ReadCapacityUnits":5,"WriteCapacityUnits":10}""")
+        }
+      }
+      "Attribute" in {
+        "should throw an assertion exception" >> {
+          Attribute("a" * 256, S) must throwA[IllegalArgumentException]
+          Attribute("", S) must throwA[IllegalArgumentException]
+        }
+        "should create correct json" in {
+          toJson(Attribute("AttributeName1", S)) must_== parse("""{"AttributeName":"AttributeName1","AttributeType":"S"}""")
+        }
+      }
+      "KeySchema" in {
+        "should create correct json" in {
+          toJson(KeySchema(Attribute("AttributeName1", S))) must_== parse("""{"HashKeyElement":{"AttributeName":"AttributeName1","AttributeType":"S"}}""")
+          toJson(KeySchema(Attribute("AttributeName1", S), Some(Attribute("AttributeName2", N)))) must_== parse("""{"HashKeyElement":{"AttributeName":"AttributeName1","AttributeType":"S"},"RangeKeyElement":{"AttributeName":"AttributeName2","AttributeType":"N"}}""")
+        }
+      }
+      "CreateTableRequest" in {
+        "should throw an assertion exception" >> {
+          CreateTableRequest("a", KeySchema(Attribute("key", S)), ProvisionedThroughPut()) must throwA[IllegalArgumentException]
+          CreateTableRequest("a" * 256, KeySchema(Attribute("key", S)), ProvisionedThroughPut()) must throwA[IllegalArgumentException]
+		  CreateTableRequest("a$a", KeySchema(Attribute("key", S)), ProvisionedThroughPut()) must throwA[IllegalArgumentException]
+        }
+        "should create correct json" in {
+          toJson(CreateTableRequest(
+              "Table1", 
+              KeySchema(Attribute("AttributeName1", S), Some(Attribute("AttributeName2", N))),
+              ProvisionedThroughPut(10, 5))) must_== parse("""{"TableName":"Table1", "KeySchema": {"HashKeyElement":{"AttributeName":"AttributeName1","AttributeType":"S"}, "RangeKeyElement":{"AttributeName":"AttributeName2","AttributeType":"N"}}, "ProvisionedThroughput":{"ReadCapacityUnits":5,"WriteCapacityUnits":10}}""")
+        }
+      }
+    }
+  }
 }
